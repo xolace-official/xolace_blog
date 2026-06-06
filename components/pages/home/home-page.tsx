5975168ee984f2339e4922533b07f69c20cb0047
Nathaniel E. Adama <kvngnathan8420@gmail.com>
2026-06-06 00:10:07 +0000

    Update home-page.tsx

--- components/pages/home/home-page.tsx
+++ components/pages/home/home-page.tsx
@@ -2,6 +2,7 @@ import { getAllPosts } from '@/lib/posts';
 import { HomePageClient } from './home-page-client';
 
 export async function HomePage() {
-    const posts = await getAllPosts();
+  const posts = await getAllPosts();
+  console.log("posts ", posts)
     return <HomePageClient posts={posts} />;
 }
